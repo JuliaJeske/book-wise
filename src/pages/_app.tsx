3966920051d211f3afa592b0502601e16edfaaba
juliajeske <julia.jeske@wehandle.com.br>
2023-04-11 15:49:06 -0300

    feat: create a structure of project

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,12 @@
-import '@/styles/globals.css'
+import '../styles/global'
+import { globalCss } from '@stitches/react'
 import type { AppProps } from 'next/app'
+
+import { Nunito } from 'next/font/google'
+
+export const nunito = Nunito({ subsets: ['latin']})
+
+globalCss()
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
