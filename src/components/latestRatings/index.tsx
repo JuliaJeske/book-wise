aa1a5de1df859fb33f865c9292824daef2a52f65
juliajeske <julia.jeske@wehandle.com.br>
2023-04-17 15:03:37 -0300

    feat: create latestRatings on project

---
+++ src/components/latestRatings/index.tsx
@@ -0,0 +1,57 @@
+import { PageTitle } from "../../components/handleComponents/PageTitle"
+import { RatingWithAuthorAndBook, RatingCard } from "../RatingCards"
+import { Text } from "../Typography"
+import { Container, LatestContainer } from "./styles"
+
+import { useQuery } from "@tanstack/react-query"
+import { api } from "@/lib/axios"
+import { useSession } from "next-auth/react"
+
+import { ChartLineUp } from "phosphor-react"
+import { Link }from "../handleComponents/Link"
+
+export const LatestRatings = () => {
+  const { data: ratings } = useQuery<RatingWithAuthorAndBook[]>(["latest-ratings"], async () => {
+    const { data } = await api.get("/ratings/latest");
+    return data?.ratings ?? []
+  })
+
+  const { data: session } = useSession();
+
+  const userId = session?.user?.id;
+
+  const { data: latestUserRating } = useQuery<RatingWithAuthorAndBook>(["latest-user-rating", userId], async () => {
+    const { data } = await api.get("/ratings/user-latest")
+    return data?.rating ?? null
+  }, {
+    enabled: !!userId
+  })
+
+  return (
+    <Container>
+      <PageTitle title="Início" icon={<ChartLineUp size={32} />} css={{
+        marginBottom: 40
+      }} />
+
+      {latestUserRating && (
+        <LatestContainer>
+          <header>
+            <Text size="sm">Sua última leitura</Text>
+
+            <Link text="Ver todas" href={`/profile/${userId}`} />
+          </header>
+
+          <RatingCard variant="compact" rating={latestUserRating} />
+        </LatestContainer>
+      )}
+
+      <Text size="sm">Avaliações mais recentes</Text>
+
+      <section>
+        {ratings?.map(rating => (
+          <RatingCard key={rating.id} rating={rating} />
+        ))}
+      </section>
+    </Container>
+  )
+}
