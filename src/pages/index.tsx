fe49a4de04bccd4e461ec3f7f9cd859245b95e01
juliajeske <julia.jeske@wehandle.com.br>
2023-04-17 15:02:46 -0300

    feat: create handle components

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,16 @@
 import { signOut, useSession } from "next-auth/react"
 import { NextPageWithLayout } from "./_app"
 import DefaultLayout from "@/layouts/DefaultLayout"
+import { HomeContainer } from "@/styles/pages/home"
+import { LatestRatings } from "@/components/latestRatings"
 
 const HomePage: NextPageWithLayout = () => {
   const {data} = useSession()
   return (
-    <pre>
-      {JSON.stringify(data, null, 2)}
-      <button onClick={() => signOut()}>Deslogar</button>
-    </pre>
+    <HomeContainer>
+      <LatestRatings />
+     
+    </HomeContainer>
   )
 }
 
